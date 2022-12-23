4a8918bb7a3584a0b66a28c0c4a6e657e34ba256
huiyingShen <hshen@ski.org>
2022-12-22 17:40:28 -0800

    starting TtsSpeaker

--- tts_speaker.py
+++ tts_speaker.py
@@ -6,16 +6,60 @@ import sys
 
 instance = vlc.Instance()
 
+class TtsSpeaker:
+    def __init__(self) -> None:
+        self.media_player = instance.media_player_new()
+        self.stopped = False
+
+    def set_media(self,txt):
+        filename = txt + ".mp3"
+        tts=gTTS(text=txt,lang="en")
+        tts.save(filename)
+        self.media = instance.media_new(filename) 
+
+
+    def playUntilDone(self):
+        self.stopped = False
+        self.media_player.set_media(self.media)
+        self.media_player.play()
+        for n in range(1000):
+            if self.stopped: 
+                self.media_player.stop()
+                break
+            sleep(0.01)
+            pos = self.media_player.get_position()
+            print(pos)
+            if pos > 0.5 and not self.media_player.is_playing(): 
+                break
+
+    def play_text(self,txt):
+        self.set_media(txt)
+        self.playUntilDone()
+        # threading.Thread(target=self.playUntilDone()).start()
+
+    def stop(self):
+        self.stopped = True
+
+    def test1(self):
+        self.play_text("testing, 1, 2, 3, ...")
 
 def text_to_media(txt = "hello, world!"):
-    tts=gTTS(text=txt,lang="en")
     filename = txt + ".mp3"
+    tts=gTTS(text=txt,lang="en")
     tts.save(filename)
+    return instance.media_new(filename) 
 
-    return vlc.Media(filename) 
+def play_text(txt,media_player):
+    if media_player is None:
+        media_player = instance.media_player_new()
+    else:
+        for n in range(1000):
+            sleep(0.01)
+            pos = media_player.get_position()
+            print(pos)
+            if pos > 0.5 and not media_player.is_playing(): 
+                break
 
-def play_text(txt = "hello, world!"):
-    media_player = instance.media_player_new()
     media_player.set_media(text_to_media(txt))
     media_player.play()
     for n in range(1000):
@@ -23,11 +67,15 @@ def play_text(txt = "hello, world!"):
         pos = media_player.get_position()
         # print("n, pos = ",n, pos)
         # if pos>0.99: return
-        if pos > 0.5 and not media_player.is_playing(): return
+        if pos > 0.5 and not media_player.is_playing(): return media_player
 
-    
+def unblock_play(txt,media_player=None):
+    threading.Thread(target=play_text,args=(txt,media_player,)).start()
+    return media_player
 if __name__ == '__main__':
-    play_text()
-    play_text("how are you doing today?")
+    TtsSpeaker().test1()
+    # media_player = unblock_play("how are you doing today?",None)
+    # unblock_play("I am fine thank you",media_player)
+    unblock_play("Am I the last one?",None)
  
     
